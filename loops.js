6a8ad00a008b5a24590f3d06627152f57c2efbcf
Ronald nunez <ronaldcuello11@gmail.com>
2018-05-24 01:47:36 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,14 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++){
+    if (i=== 1){
+    array.push("I am 1 strange loop.");
+  }  else{array.push("I am ${i} strange loops.")
+  }
+}
+return array;
+}
+function doWhileLoop(n){
+  let countdown = 100
+  while (countdown > (n)) {
+    console.log(--countdown)
+}
